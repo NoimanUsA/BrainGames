0b2510623942aab669a86aeb1b58a8643250fea6
Noiman <noiman762@gmail.com>
2019-11-13 19:18:39 +0300

    brain-even

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import readlineSync from 'readline-sync';
 
 
+const userName = () => {
+  console.log('Welcome to Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}! `);
+  return true;
+};
 
-const userName = function (){
+const even = () => {
+  console.log('Welcome to Brain the Games! \n'
+    + 'Answer "yes" if the number is even, otherwise answer "no" ');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}! \n`);
 
-console.log('Welcome to Brain Games! \n'); 
-const name  = readlineSync.question('May I have your name? \n');
-console.log('Hello ' + name +'!');
+  const iter = (count) => {
+    if (count === 3) {
+      console.log(`Congratulations, ${name}!`);
+      return true;
+    }
+    const num = Math.floor(Math.random() * 100 + 1);
+    console.log(`Question: ${num}`);
+    const answer = readlineSync.question('Your answer: ');
+    const rigthAnswer = num % 2 === 0 ? 'yes' : 'no';
+    if (answer !== rigthAnswer) {
+      console.log(`'${answer}' is wrong answer, corrent answer was '${rigthAnswer}' \n Let's try again, ${name}`);
+      return false;
+    }
 
-}
-export default userName;
+    console.log('Correct!');
+    return iter(count + 1);
+  };
+
+  iter(0);
+};
+
+export { userName, even };
